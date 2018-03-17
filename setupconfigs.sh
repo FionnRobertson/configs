500d63e22afefea9747b5e164875f1bad68ea17b
FionnRobertson <fionnrobertson@googlemail.com>
2018-03-17 16:06:21 +0000

    Bugfixing

--- setupconfigs.sh
+++ setupconfigs.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 # Move solarized.vim to .vim/colors
+if [ ! -d "~/.vim/colors" ]; then
+    mkdir ~/.vim/colors
+fi
+
 mv solarized.vim ~/.vim/colors/solarized.vim
 mv .vimrc ~/.vimrc
 mv config ~/.ssh/config
